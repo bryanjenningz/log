40b06a29afd1846774af7366df68056a558f4fb4
Bryan Jennings <bryanjenningz@gmail.com>
2016-10-03 10:39:04 -0700

    make editing input styling same width and height as entry width and height

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
 import {render} from 'react-dom'
 import range from 'lodash/range'
 
@@ -21,10 +21,18 @@ class App extends Component {
     this.setState({editing: null})
   }
 
+  currentlyEditing(x, y) {
+    return Array.isArray(this.state.editing) && this.state.editing[0] === x && this.state.editing[1] === y
+  }
+
+  updateEntry(x, y, value) {
+    this.setState({rows: this.state.rows.map((row, j) => row.map((entry, i) => i === x && j === y ? value : entry))})
+  }
+
   render() {
     return (
       <div>
-        <table className="table table-striped table-hover">
+        <table className="table table-striped table-hover table-bordered">
           <thead>
             <tr>
               {this.state.headers.map((header, i) => (<th key={i}>{header}</th>))}
@@ -34,9 +42,13 @@ class App extends Component {
             {this.state.rows.map((row, j) => (
               <tr key={j}>
                 {row.map((entry, i) => (
-                  <td key={i} onMouseUp={() => this.edit(i, j)}>
-                    {Array.isArray(this.state.editing) && this.state.editing[0] === i && this.state.editing[1] === j
-                      ? <InputEntry onBlur={() => this.cancelEdit()} />
+                  <td
+                    key={i}
+                    onMouseUp={() => this.edit(i, j)}
+                    className="col-xs-1"
+                    style={Object.assign({position: 'relative'}, this.currentlyEditing(i, j) ? {padding: 0} : {})}>
+                    {this.currentlyEditing(i, j)
+                      ? <InputEntry handleUpdate={(value) => this.updateEntry(i, j, value)} handleBlur={() => this.cancelEdit()} />
                       : entry
                     }
                   </td>
@@ -56,11 +68,31 @@ class InputEntry extends Component {
     this.refs.input.focus()
   }
 
+  handleSubmit(e, value) {
+    e.preventDefault()
+    if (value.trim()) {
+      this.props.handleUpdate(value.trim())
+    }
+  }
+
   render() {
     return (
-      <input ref="input" onBlur={this.props.onBlur} />
+      <form 
+        onSubmit={e => this.handleSubmit(e, this.refs.input.value)}
+        style={{position: 'absolute', top: 0, bottom: 0}}>
+        <input
+          className="form-control"
+          ref="input"
+          onBlur={this.props.handleBlur}
+          style={{height: '100%'}} />
+      </form>
     )
   }
+}
+
+InputEntry.propTypes = {
+  handleUpdate: PropTypes.func.isRequired,
+  handleBlur: PropTypes.func.isRequired,
 }
 
 
